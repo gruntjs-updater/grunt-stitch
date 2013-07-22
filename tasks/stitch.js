6313690236ebdb5c78b8fee62066f1a19220abcc
phaneendra <phaneendra.krishnareddy@gmail.com>
2013-07-22 10:42:54 +0800

    multiple targets and alias support

--- tasks/stitch.js
+++ tasks/stitch.js
@@ -8,41 +8,91 @@
 
 'use strict';
 
-var fs = require('fs'),
-    stitch = require('stitch'),
-    gruntRef;
+var path = require('path');
+var stitch = require('stitch');
 
 module.exports = function(grunt) {
-  gruntRef = grunt;
 
-  grunt.registerTask('stitch', 'Compile common.js modules with stitch.', function() {
-    var done = this.async();
+    grunt.registerMultiTask('stitch', 'Compile common.js modules with stitch.', function() {
+        var done = this.async();
 
-    var options = this.options();
+        var options = this.options();
+        var tmpDir = __dirname + options.tmp;
+        var dependencies = grunt.file.expand(options.dependencies);
+        var aliasregx = new RegExp('^(' + grunt.util._.keys(options.paths).join('|') + ')');
 
-    //try {
-      //var _ = require('underscore');
-      //stitch.compilers.ejs = function(module, filename) {
-        //var content = _.template(fs.readFileSync(filename, 'utf8')).source;
-        //module._compile('module.exports = '+content, filename);
-      //};
-    //}
-    //catch (err) {}
+        // Iterate over all specified file groups.
+        this.files.forEach(function(f) {
 
-    stitch.createPackage(options).compile(function(err, source) {
-      if (err) {
-        grunt.log.error(err);
-      }
+            var pathMaps = f.src.filter(function(filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function(filepath) {
+                var matches = filepath.match(aliasregx);
+                var dest = filepath;
+                if (matches) {
+                    dest = filepath.replace(matches[0], options.paths[matches[0]]);
+                }
+                return [filepath, dest];
+            });
 
-      fs.writeFile(options.dest, source, function(err) {
-        if (err) {
-          grunt.log.error(err);
-        } else {
-          grunt.log.writeln("File \""+options.dest+"\" created.");
+            // Copy over any NPM dependencies, so they can be `require`d in a sexy way.
+            pathMaps = pathMaps.concat(grunt.util._.map(options.npmDependencies, function(src, module) {
+                var filepath = path.normalize('node_modules/' + module + '/' + src);
+                return [filepath, module + '.js'];
+            }));
+
+            // Clean the tmp dir, to prevent picking up old files.
+            if (grunt.file.exists(tmpDir)) {
+                grunt.file.delete(tmpDir);
+            }
+
+            // Copy everything to the tmp directory, which will be the
+            // base path for the Stitch bundle.
+            pathMaps.forEach(function(pathMap) {
+                grunt.file.copy(pathMap[0], tmpDir + '/' + pathMap[1]);
+            });
+
+            assertFiles(pathMaps.length, tmpDir, function() {
+                // Create the Stitch package.
+                stitch.createPackage({
+                    paths: [tmpDir],
+                    dependencies: dependencies
+                }).compile(function(err, source) {
+                    if (err) {
+                        return done(err);
+                    }
+                    grunt.file.write(f.dest, source);
+                    grunt.log.writeln('File "' + f.dest + '" created.');
+                    done();
+                });
+            });
+        });
+    });
+
+    /**
+    * Sometimes, the Stitch compliation appears to happen before all files are copied over to
+    * the `tmpDir`.  We simply wait until they are.
+    */
+    function assertFiles(expectedNumFiles, tmpDir, callback) {
+        function countFiles() {
+            var numFiles = 0;
+            grunt.file.recurse(tmpDir, function(abspath, rootdir, subdir, filename) {
+                numFiles++;
+            });
+            return numFiles === expectedNumFiles;
         }
 
-        done();
-      });
-    });
-  });
+        var interval = setInterval(function() {
+            if (countFiles()) {
+                clearInterval(interval);
+                callback();
+            }
+        }, 100);
+    }
 };
--- test/stitch_test.js
+++ test/stitch_test.js
@@ -30,19 +30,10 @@ exports.stitch = {
   default_options: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    var actual = grunt.file.read('tmp/sample/out.js');
+    var expected = grunt.file.read('test/expected/sample/out.js');
+    test.equal(actual, expected, 'should generate a proper output file.');
 
     test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
-
-    test.done();
-  },
+  }
 };
